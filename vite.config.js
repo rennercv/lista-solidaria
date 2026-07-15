d06fe2d5a08dd58aa26b00c5809eebf6afde48c6
Renner Carara Valfré <rennercararavalfre@hotmail.com>
2026-07-15 01:20:27 -0300

    Adiciona build com múltiplas páginas (index e admin)

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+
+export default defineConfig({
+
+    build: {
+
+        rollupOptions: {
+
+            input: {
+
+                main: resolve(__dirname, "index.html"),
+
+                admin: resolve(__dirname, "admin.html")
+
+            }
+
+        }
+
+    }
+
+});
